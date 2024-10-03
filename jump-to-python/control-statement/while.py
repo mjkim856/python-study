248aa94eb27ccf277334cb28f37cf06eaa602a91
mjkim856 <mjkim856@gmail.com>
2024-10-04 01:38:50 +0900

    test: vscode to github

--- jump-to-python/control-statement/while.py
+++ jump-to-python/control-statement/while.py
@@ -4,7 +4,7 @@ while treehit < 10:
     treehit = treehit + 1   
     print("%d treehit!" % treehit)
     if treehit == 10:
-        print("The tree falls")
+        print("The tree falls")  
 
 print()
 
