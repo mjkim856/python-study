5ac131dd100ce4b02b255d9f534cc26903bf89ba
mjkim856 <mjkim856@gmail.com>
2024-10-04 01:37:37 +0900

    test: vscode to github

--- jump-to-python/control-statement/while.py
+++ jump-to-python/control-statement/while.py
@@ -1,7 +1,7 @@
 # 예시 1
-treehit = 0
+treehit = 0  
 while treehit < 10:
-    treehit = treehit + 1
+    treehit = treehit + 1   
     print("%d treehit!" % treehit)
     if treehit == 10:
         print("The tree falls")
