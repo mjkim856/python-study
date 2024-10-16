2b127fa9abb3ad029f862d5705bd414dac97e899
mjkim856 <mjkim856@gmail.com>
2024-10-16 13:30:20 +0900

    learn: 입출력 정리

---
+++ jump-to-python/3. input-output-function/input-output.py
@@ -0,0 +1,14 @@
+# 입력값 활용하기
+'''
+a = input("값을 입력해주세요: ")    # 입력값은 string으로 취급된다
+print(a)
+'''
+
+print()
+
+# 프린트 심층 학습
+print("life" "is" "too short")      # + 없이도 이렇게 적으면 문자열 더하기 기능 됨
+print("life", "is", "too short")    # , 넣으면 띄어쓰기로 작동함
+
+for i in range(10):
+    print(i, end = ' ')             # 한 줄에 결과값을 출력하기 위해 매개변수 end 사용
