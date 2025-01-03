aea1707603382a7852ed16f125143e04ec1f1913
mjkim856 <mjkim856@gmail.com>
2025-01-03 14:58:27 +0900

    learn: 네이버 검색 API를 사용한 검색 결과 print() 출력 구현

---
+++ fastcampus-47-project/3-crawling/02_function_call.py
@@ -0,0 +1,9 @@
+import requests
+
+url = "https://openapi.naver.com/v1/search/blog.json?query=과천 맛집&start=10000&display=5"
+res = requests.get(url, headers={"X-Naver-Client-Id": "pNdISaOoIKomHA0fPeSu",
+                                 "X-Naver-Client-Secret": "h0ypZof0aY"})
+print(res.json())
+r = res.json()
+
+# print(r['items']) << json()은 딕셔너리 형식으로 출력되어서, 이런 식으로 원하는 항목을 확인할 수 있다. 
